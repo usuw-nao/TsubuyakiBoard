ffb28d859421fb0238e8a41a8e0a701036da638c
usui <ctct@gmail.com>
2022-07-20 12:50:33 +0900

    Logic削除

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.IOException;
 
+
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -11,9 +12,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import model.GetMutterListLogic;
+import dao.DaoFactory;
+import dao.MutterDao;
+
 import model.Mutter;
-import model.PostMutterLogic;
+
 import model.User;
 
 /**
@@ -30,10 +33,11 @@ public class Main extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		GetMutterListLogic getMutterListLogic = new GetMutterListLogic();
+		//GetMutterListLogic getMutterListLogic = new GetMutterListLogic();
 		try {
-			List<Mutter> mutterList = getMutterListLogic.execute();
-
+			//List<Mutter> mutterList = getMutterListLogic.execute();
+			MutterDao mutterDao=DaoFactory.createMutterDao();
+			List<Mutter> mutterList=mutterDao.findAll();
 			request.setAttribute("mutterList", mutterList);
 
 			HttpSession session = request.getSession();
@@ -53,18 +57,21 @@ public class Main extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		String text = request.getParameter("text");
+		request.setCharacterEncoding("UTF-8");		
+
 		try {
+			String text = request.getParameter("text");
+			MutterDao mutterDao=DaoFactory.createMutterDao();
+		List<Mutter> mutterList=mutterDao.findAll();
+		request.setAttribute("mutterList", mutterList);
 
 			if (text != null && text.length() != 0) {
 				HttpSession session = request.getSession();
 				User loginUser = (User) session.getAttribute("loginUser");
 
-				Mutter mutter = new Mutter(0, loginUser.getName(), text);
-				PostMutterLogic postmutterlogic = new PostMutterLogic();
-
-				postmutterlogic.execute(mutter);
+				//Mutter mutter = new Mutter(0, loginUser.getName(), text);
+				//PostMutterLogic postmutterlogic = new PostMutterLogic();
+				//postmutterlogic.execute(mutter);
 
 			} else {
 				request.setAttribute("errorMsg", "つぶやきが入力されていません");
@@ -73,10 +80,12 @@ public class Main extends HttpServlet {
 			e.printStackTrace();
 		}
 		try {
-			GetMutterListLogic getMutterListLogic = new GetMutterListLogic();
-			List<Mutter> mutterList;
-			mutterList = getMutterListLogic.execute();
+			//GetMutterListLogic getMutterListLogic = new GetMutterListLogic();
+			//List<Mutter> mutterList;
+			//mutterList = getMutterListLogic.execute();
 
+			MutterDao mutterDao=DaoFactory.createMutterDao();
+			List<Mutter> mutterList=mutterDao.findAll();
 			request.setAttribute("mutterList", mutterList);
 			request.getRequestDispatcher("/WEB-INF/view/main.jsp").forward(request, response);
 		} catch (Exception e) {
